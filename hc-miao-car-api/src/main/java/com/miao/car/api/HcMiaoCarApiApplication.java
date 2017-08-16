bb2a1489a121cab0f8630c9892945211593b0297
turbo.yang <turbo.yang@huochaicf.com>
2017-08-16 20:36:34 +0800

    base project

--- hc-miao-car-api/src/main/java/com/miao/car/api/HcMiaoCarApiApplication.java
+++ hc-miao-car-api/src/main/java/com/miao/car/api/HcMiaoCarApiApplication.java
@@ -3,9 +3,11 @@ package com.miao.car.api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableConfigServer
+@EnableEurekaServer
 public class HcMiaoCarApiApplication {
 
 	public static void main(String[] args) {
--- hc-miao-car-cms/src/main/java/com/miao/car/cms/HcMiaoCarCmsApplication.java
+++ hc-miao-car-cms/src/main/java/com/miao/car/cms/HcMiaoCarCmsApplication.java
@@ -2,10 +2,18 @@ package com.miao.car.cms;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class HcMiaoCarCmsApplication {
 
+	@Bean
+	public RestTemplate restTemplate() {
+		return new RestTemplate();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(HcMiaoCarCmsApplication.class, args);
