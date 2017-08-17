c20d828b224920388371d79f5a63bdee9b852450
turbo.yang <turbo.yang@huochaicf.com>
2017-08-17 14:47:44 +0800

    base project

--- hc-miao-car-cms/src/main/java/com/miao/car/cms/HcMiaoCarCmsApplication.java
+++ hc-miao-car-cms/src/main/java/com/miao/car/cms/HcMiaoCarCmsApplication.java
@@ -3,11 +3,13 @@ package com.miao.car.cms;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableFeignClients
 public class HcMiaoCarCmsApplication {
 
 	@Bean
--- hc-miao-car-cms/src/main/java/com/miao/car/cms/HelloController.java
+++ hc-miao-car-cms/src/main/java/com/miao/car/cms/HelloController.java
@@ -1,6 +1,7 @@
 package com.miao.car.cms;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,6 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
  * Created by xsing on 2017/8/16.
  */
 @RestController
+@RefreshScope
 public class HelloController {
     @Value("${weixin.wx.key}")
     String  appId;
