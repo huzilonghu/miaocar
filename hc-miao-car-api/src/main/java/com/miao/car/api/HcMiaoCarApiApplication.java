c2b073e380fc862201a3af7a1e88f6603928e6e8
turbo.yang <turbo.yang@huochaicf.com>
2017-08-22 13:17:11 +0800

    base project

--- hc-miao-car-api/src/main/java/com/miao/car/api/HcMiaoCarApiApplication.java
+++ hc-miao-car-api/src/main/java/com/miao/car/api/HcMiaoCarApiApplication.java
@@ -5,6 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+import java.util.List;
+
 @SpringBootApplication
 @EnableConfigServer
 @EnableEurekaServer
